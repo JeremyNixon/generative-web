f4c7dc20be022eac2b876f6cac52549e2e2642d9
Amy Deng <dengk@berkeley.edu>
2023-12-02 17:07:18 -0800

    openai call

--- content.js
+++ content.js
@@ -25,3 +25,41 @@ document.addEventListener('click', rewriteText);
 //     }
 //   });
   
+
+async function requestOpenai(originalText, userPrompt) {
+  // Construct the data to be sent to the OpenAI API
+  const data = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "system", content: `Here's the original text: "${originalText}". Rewrite it according to the following user instruction: "${userPrompt}". Make sure the output is similar in length as the original text.` }],
+    max_tokens: length(originalText) * 1.5
+  };
+
+  // Make an asynchronous request to the OpenAI API
+  // Note: You need to include your OpenAI API Key in the request headers
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer 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`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  });
+
+  const responseData = await response.json();
+  console.log("responseData: ", responseData)
+  return responseData.choices[0]["message"]["content"];
+}
+
+
+async function rewriteTextNodes(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    if (node.textContent.trim() !== '') {
+      const rewrittenText = await rewriteTextInShakespeareStyle(node.textContent);
+      node.textContent = rewrittenText;
+    }
+  } else {
+    for (const child of node.childNodes) {
+      await rewriteTextNodes(child); // Ensure recursive calls are awaited
+    }
+  }
+}
