7ea2f24a3dea609a50e5fde597c33c6490467bd6
pavanreddy21 <pavanreddygottimukkula@gmaail.com>
2023-12-02 17:13:02 -0800

    Save prompt to local storage

--- popup.js
+++ popup.js
@@ -1,8 +1,23 @@
 document.getElementById('sendPrompt').addEventListener('click', () => {
-    console.log("Calling Rewrite Text!!!")
-    const userPrompt = document.getElementById('userPrompt').value;
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, {action: 'rewriteText', prompt: userPrompt});
-    });
+  console.log("Calling Rewrite Text!!!")
+  const userPrompt = document.getElementById('userPrompt').value;
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'rewriteText', prompt: userPrompt });
   });
-  
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Retrieve the stored value from LocalStorage
+  const storedPrompt = localStorage.getItem('userPrompt');
+  if (storedPrompt) {
+    document.getElementById('userPrompt').value = storedPrompt;
+  }
+
+  // Event listener for changes in the textarea
+  document.getElementById('userPrompt').addEventListener('input', function (event) {
+    // Save the current value to LocalStorage
+    localStorage.setItem('userPrompt', event.target.value);
+  });
+});
